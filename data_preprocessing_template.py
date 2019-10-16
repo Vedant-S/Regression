e26ad3ed734f7754780162144be7c5b3d070ac8f
Vedant-S <55291816+Vedant-S@users.noreply.github.com>
2019-10-16 21:56:33 +0530

    Update data_preprocessing_template.py

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -10,9 +10,7 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
